75eeea748d247ded44dbd55c676a832ffd1bc57b
Lokendra Nath <nathdeveloper25@gmail.com>
2026-03-27 22:55:02 +0530

    create simple ping pong example

--- 16.1-websocket/client/src/App.tsx
+++ 16.1-websocket/client/src/App.tsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+  const [message, setMessage] = useState("");
+
+  function sendMessage() {
+    if (socket && socket.readyState === WebSocket.OPEN) {
+      socket.send(message);
+    } else {
+      alert("Socket is not Connected Yet!");
+    }
+  }
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8000");
+
+    setSocket(ws);
+    ws.onmessage = (ev) => {
+      console.log(ev.data);
+    };
+  }, []);
+
   return (
     <>
-      <h1>Hey</h1>
+      <h1>Hey This is Exmaple</h1>
+      <input
+        type="text"
+        placeholder="Enter Message"
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button onClick={sendMessage}>Send Message</button>
     </>
   );
 }
