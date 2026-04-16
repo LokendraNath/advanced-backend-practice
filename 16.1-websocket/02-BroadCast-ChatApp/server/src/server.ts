8870553767482b66e3320a14616a550e45466d17
Lokendra Nath <nathdeveloper25@gmail.com>
2026-04-16 23:32:04 +0530

    setup backend express and cors and initial setup

--- 16.1-websocket/02-BroadCast-ChatApp/server/src/server.ts
+++ 16.1-websocket/02-BroadCast-ChatApp/server/src/server.ts
@@ -1,39 +1,64 @@
 import { WebSocketServer, WebSocket } from "ws";
+import express from "express";
+import cors from "cors";
 
+import mongoose from "mongoose";
+import { Group } from "./models/groupModel.js";
+
+const app = express();
 const wss = new WebSocketServer({ port: 8080 });
+
+app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+  }),
+);
+app.use(express.json());
 
 interface User {
   socket: WebSocket;
   room: string;
 }
 
-let connectedUsers: User[] = [];
+mongoose
+  .connect("mongodb://localhost:27017/chatAppWS")
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
-wss.on("connection", (socket) => {
-  socket.on("message", (msg) => {
-    const messageData = JSON.parse(msg.toString());
-
-    if (messageData.type === "join") {
-      connectedUsers.push({
-        socket,
-        room: messageData.payload.roomId,
-      });
-    }
-
-    if (messageData.type === "chat") {
-      const senderRoomId = connectedUsers.find(
-        (s) => s.socket === socket,
-      )?.room;
-
-      connectedUsers.forEach((user) => {
-        if (user.room === senderRoomId) {
-          user.socket.send(messageData.payload.message);
-        }
-      });
-    }
-  });
-
-  socket.on("close", () => {
-    connectedUsers = connectedUsers.filter((u) => u.socket !== socket);
-  });
+app.post("/createGroup", async (req, res) => {
+  const { groupId, groupName } = req.body;
+  const groupExist = await Group.find({ groupId });
+  if (groupExist) return res.json();
 });
+
+// wss.on("connection", (socket) => {
+//   socket.on("message", (msg) => {
+//     const messageData = JSON.parse(msg.toString());
+
+//     if (messageData.type === "join") {
+//       connectedUsers.push({
+//         socket,
+//         room: messageData.payload.roomId,
+//       });
+//     }
+
+//     if (messageData.type === "chat") {
+//       const senderRoomId = connectedUsers.find(
+//         (s) => s.socket === socket,
+//       )?.room;
+
+//       connectedUsers.forEach((user) => {
+//         if (user.room === senderRoomId) {
+//           user.socket.send(messageData.payload.message);
+//         }
+//       });
+//     }
+//   });
+
+//   socket.on("close", () => {
+//     connectedUsers = connectedUsers.filter((u) => u.socket !== socket);
+//   });
+// });
+
+app.listen(3030, () => console.log("App Listen On 3030"));
