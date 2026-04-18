b1b1a8215dbbcaa18a499ace732f1d38aa8b54f6
Lokendra Nath <nathdeveloper25@gmail.com>
2026-04-18 22:50:15 +0530

    fix cors issue

--- 16.1-websocket/02-BroadCast-ChatApp/server/src/server.ts
+++ 16.1-websocket/02-BroadCast-ChatApp/server/src/server.ts
@@ -11,7 +11,10 @@ const wss = new WebSocketServer({ port: 8080 });
 app.use(cors());
 app.use(
   cors({
-    origin: "http://localhost:5173/",
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 app.use(express.json());
